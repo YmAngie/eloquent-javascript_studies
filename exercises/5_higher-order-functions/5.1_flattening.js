094f4698137921a79bc65c2afc0753bf45303c39
Angelica Ymydykova <aymydykova@oggettoweb.com>
2018-04-19 15:03:12 +0300

    Add Flatenning solution (5.1)

---
+++ exercises/5_higher-order-functions/5.1_flattening.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+let flattenedArray = arrays.reduce(function(flattened, current) {
+  return flattened.concat(current);
+});
+                                   
+console.log(flattenedArray);
